3334e8644793afc138e74d801f28bd41cfd7ef89
ivan <block4ain226@gmail.com>
2024-10-22 12:38:11 +0300

    feat: create user dto with validation

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,25 @@
+import { IsEmail, IsOptional, IsPhoneNumber, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(20)
+  username?: string;
+  @IsEmail()
+  email: string;
+  @Matches(/^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{4,}$/, { message: 'password should have min 4 characters and min 1 uppercase' })
+  password: string;
+  @IsPhoneNumber()
+  phone: string;
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(20)
+  firstname?: string;
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(20)
+  lastname?: string;
+}
